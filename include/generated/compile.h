5530dcb8c3750170abf88657a00b2b7b5be8f042
MassStash <MassStash@gmail.com>
2015-01-27 22:30:13 -0600

    Initial source drop

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jan 15 12:38:37 CST 2015"
+#define LINUX_COMPILE_BY "Aaron"
+#define LINUX_COMPILE_HOST "kernel1-build2"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
